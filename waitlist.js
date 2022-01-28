930e6a5f67528c2cddab108ef9cd1a3eef2e7d6e
Jon Chen <jonathan.chen.6799@gmail.com>
2022-01-27 23:12:27 -0500

    csv

--- waitlist.js
+++ waitlist.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 var user = require('./models/userModel')
 var app = express();
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 // const port = 3000;
 app.set('view engine', 'ejs');
 
@@ -36,17 +37,27 @@ app.get('/success', function(req, res) {
 })
 
 app.post('/signup', async function(req, res) {
-    user.create({
-        email : req.body.email
-    }, 
-    function(err, data) {
-        if(err){
-            res.send(err)
-        } else {
-            // res.redirect('/success');
-            res.redirect('https://www.facebook.com/jonthan.chen');
-        }
-    })
+    const csvWriter = createCsvWriter({
+    path: 'email.csv',
+    header: [
+        {id: 'email', title: 'Name'},
+    ]
+    });
+    const data = [{email: req.body.email}]
+    csvWriter
+    .writeRecords(data)
+    .then(()=> console.log('The CSV file was written successfully'));
+    // user.create({
+    //     email : req.body.email
+    // }, 
+    // function(err, data) {
+    //     if(err){
+    //         res.send(err)
+    //     } else {
+    //         // res.redirect('/success');
+    //         res.redirect('https://www.facebook.com/jonthan.chen');
+    //     }
+    // })
 })
 
 app.listen(process.env.PORT || 5000)
